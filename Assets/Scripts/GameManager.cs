9dd0257fd86365a5401d23836294ae13983d9722
pengHappy <zp007@zp007deMacBook-Pro.local>
2017-04-25 10:14:53 -0700

    MatchStick Run Game, final version

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public float levelStartDelay = 2f;
+
+	public int score = 0;
+
+	[HideInInspector] public bool isGround = true;
+	[HideInInspector] public int level = 0;
+
+	private bool restart = true;
+	private GameObject resumeButton;
+	private Text levelText;
+	private GameObject playerObject;
+	private GameObject levelImage;
+	private BoardManager boardScript;
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		boardScript = GetComponent<BoardManager> ();
+	}
+
+	void OnLevelWasLoaded(int index)
+	{
+		if (restart) {
+			restart = false;
+			level = 0;
+			score = 0;
+		} else {
+			level++;
+		}
+		//Call InitGame to initialize our level.
+		InitGame();
+	}
+
+	void InitGame() {
+		playerObject = GameObject.Find ("Player");
+		playerObject.SetActive (false);
+
+		resumeButton = GameObject.Find ("RestartButton");
+		resumeButton.SetActive (false);
+
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+		levelText.text = "Level " + (level + 1);
+		StartCoroutine(goAfterDelay());
+	}
+
+	//Hides black image used between levels
+	void HideLevelImage()
+	{
+		//Disable the levelImage gameObject.
+		levelImage.SetActive(false);
+	}
+
+	public void GameOver() {
+		levelText.text = "Game Over";
+		levelImage.SetActive (true);
+		resumeButton.SetActive (true);
+		enabled = false;
+		restart = true;
+	}
+
+	public void RestartGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+	}
+
+	IEnumerator goAfterDelay() {
+		levelImage.SetActive (true);
+		yield return new WaitForSecondsRealtime(levelStartDelay);
+		levelImage.SetActive (false);
+		boardScript.SetupScene (level);
+		playerObject.SetActive (true);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+	public float restartLevelDelay = 1f;
+	public Text LevelText;
+	public Text ScoreText;
+	public Text HPText;
+
+	private Rigidbody2D rb2d;
+	private int HP = 100;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		LevelText.text = "Level " + (GameManager.instance.level + 1);
+		ScoreText.text = "Score: " + GameManager.instance.score;
+		HPText.text = "HP: " + HP;
+	}
+
+	void OnCollisionStay2D(Collision2D collision) {
+		if (!GameManager.instance.isGround && collision.collider.tag == "Ground" && rb2d.velocity.y == 0) {
+			GameManager.instance.isGround = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "NextLevel") {
+			//Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+			Invoke ("Restart", restartLevelDelay);
+
+			//Disable the player object since level is over.
+			enabled = false;
+		} else if (other.tag == "Coin") {
+			GameManager.instance.score += 10;
+			HP += 10;
+			other.gameObject.SetActive (false);
+		} else if (other.tag == "Obstacle") {
+			HP -= 30;
+			other.gameObject.SetActive (false);
+			CheckIfGameOver ();
+		} else if (other.tag == "Death") {
+			HP = 0;
+			CheckIfGameOver ();
+		}
+		ScoreText.text = "Score: " + GameManager.instance.score;
+		HPText.text = "HP: " + HP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool isJump = false;
+	#if UNITY_STANDALONE || UNITY_WEBPLAYER
+		isJump = Input.GetKeyDown ("space");
+	#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+		isJump = (Input.touchCount > 0);
+	#endif
+
+		if (GameManager.instance.isGround && isJump) {
+			rb2d.AddForce (new Vector2 (0, 1400));
+			GameManager.instance.isGround = false;
+		}
+	}
+
+	//Restart reloads the scene when called.
+	private void Restart ()
+	{
+		//Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
+		//and not load all the scene object in the current scene.
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+	}
+
+	private void CheckIfGameOver ()
+	{
+		//Check if food point total is less than or equal to zero.
+		if (HP <= 0) 
+		{
+			LevelText.text = "Game Over";
+			//Call the GameOver function of GameManager.
+			GameManager.instance.GameOver ();
+		}
+	}
+}
